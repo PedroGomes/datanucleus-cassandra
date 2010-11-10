905086ac8a60d5811ab4e1066f30d0d41c9555fb
pedro <pedro@192.168.1.3>
2010-11-10 23:26:38 +0000

    -Support to small = operations based on indexes. -ranged slices method is now used

--- src/org/datanucleus/store/cassandra/query/CassandraQuery.java
+++ src/org/datanucleus/store/cassandra/query/CassandraQuery.java
@@ -1,14 +1,21 @@
 package org.datanucleus.store.cassandra.query;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
+import org.apache.cassandra.thrift.ColumnOrSuperColumn;
 import org.apache.cassandra.thrift.ColumnParent;
 import org.apache.cassandra.thrift.ConsistencyLevel;
 import org.apache.cassandra.thrift.InvalidRequestException;
+import org.apache.cassandra.thrift.KeyRange;
 import org.apache.cassandra.thrift.KeySlice;
 import org.apache.cassandra.thrift.SlicePredicate;
+import org.apache.cassandra.thrift.SliceRange;
 import org.apache.cassandra.thrift.TimedOutException;
 import org.apache.cassandra.thrift.UnavailableException;
 import org.apache.cassandra.thrift.Cassandra.Client;
@@ -28,11 +35,14 @@ import org.datanucleus.store.cassandra.CassandraUtils;
 
 public class CassandraQuery {
 
-	public static int search_slice_ratio = 1000; //should come from the properties  
+	public static int search_slice_ratio = 1000; // should come from the
+
+	// properties
 
 	static List getObjectsOfCandidateType(final ExecutionContext ec,
 			final CassandraManagedConnection mconn, Class candidateClass,
-			boolean subclasses, boolean ignoreCache,long fromInclNo , long toExclNo) {
+			boolean subclasses, boolean ignoreCache, long fromInclNo,
+			long toExclNo) {
 		List results = new ArrayList();
 
 		try {
@@ -58,39 +68,39 @@ public class CassandraQuery {
 			slice_predicate.setColumn_names(column_names);
 
 			String last_key = "";
-			int number_keys =0;
-            boolean terminated = false;
-            
-            
-            
-		    long limit = toExclNo;//(toExclNo<0) ? -1 : (toExclNo-1);             
-            List<KeySlice> result = new ArrayList<KeySlice>();
+			int number_keys = 0;
+			boolean terminated = false;
 
-            while (!terminated) {
-                List<KeySlice> keys = c.get_range_slice(keyspace, parent, slice_predicate, last_key, "", search_slice_ratio, ConsistencyLevel.QUORUM);
-               
-                if (!keys.isEmpty()) {
-                    last_key = keys.get(keys.size() - 1).key;
-                }
-                
-                for (KeySlice key : keys) {
-                	if(!key.getColumns().isEmpty()){
-                		number_keys++;
-						if(number_keys>fromInclNo){
-						result.add(key);
+			long limit = toExclNo;// (toExclNo<0) ? -1 : (toExclNo-1);
+			List<KeySlice> result = new ArrayList<KeySlice>();
+
+			while (!terminated) {
+				List<KeySlice> keys = c.get_range_slice(keyspace, parent,
+						slice_predicate, last_key, "", search_slice_ratio,
+						ConsistencyLevel.QUORUM);
+
+				if (!keys.isEmpty()) {
+					last_key = keys.get(keys.size() - 1).key;
+				}
+
+				for (KeySlice key : keys) {
+					if (!key.getColumns().isEmpty()) {
+						number_keys++;
+						if (number_keys > fromInclNo) {
+							result.add(key);
 						}
-						
-                	}
-                	if (number_keys >= limit) {
-                        terminated = true;
-                        break;
-                    }
-                	
-                }
-                if (keys.size() < search_slice_ratio) {
-                    terminated = true;
-                } 
-            }
+
+					}
+					if (number_keys >= limit) {
+						terminated = true;
+						break;
+					}
+
+				}
+				if (keys.size() < search_slice_ratio) {
+					terminated = true;
+				}
+			}
 
 			Iterator<KeySlice> iterator = result.iterator();
 
@@ -108,19 +118,281 @@ public class CassandraQuery {
 								}
 
 								@Override
-								public void fetchNonLoadedFields(ObjectProvider sm) {
-									sm.replaceNonLoadedFields(acmd.getAllMemberPositions(),new CassandraFetchFieldManager(acmd, sm, keySlice.getColumns()));
+								public void fetchNonLoadedFields(
+										ObjectProvider sm) {
+									sm.replaceNonLoadedFields(acmd
+											.getAllMemberPositions(),
+											new CassandraFetchFieldManager(
+													acmd, sm, keySlice
+															.getColumns()));
 								}
 
 								@Override
 								public void fetchFields(ObjectProvider sm) {
-									sm.replaceFields(acmd.getPKMemberPositions(),new CassandraFetchFieldManager(acmd, sm, keySlice.getColumns()));
-									sm.replaceFields(acmd.getBasicMemberPositions(clr, ec.getMetaDataManager()),new CassandraFetchFieldManager(acmd, sm, keySlice.getColumns()));
+									sm.replaceFields(acmd
+											.getPKMemberPositions(),
+											new CassandraFetchFieldManager(
+													acmd, sm, keySlice
+															.getColumns()));
+									sm.replaceFields(acmd
+											.getBasicMemberPositions(clr, ec
+													.getMetaDataManager()),
+											new CassandraFetchFieldManager(
+													acmd, sm, keySlice
+															.getColumns()));
 
 								}
 							}, ignoreCache, true));
 
 				}
+			}
+		} catch (InvalidRequestException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		} catch (UnavailableException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		} catch (TimedOutException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		} catch (TException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		}
+		return results;
+	}
+
+	static List getObjectsOfCandidateType(final ExecutionContext ec,
+			final CassandraManagedConnection mconn, Class candidateClass,
+			boolean subclasses, boolean ignoreCache, String fromInclNo,
+			String toExclNo) {
+		List results = new ArrayList();
+
+		try {
+			String keyspace = ((CassandraStoreManager) ec.getStoreManager())
+					.getConnectionInfo().getKeyspace();
+			final ClassLoaderResolver clr = ec.getClassLoaderResolver();
+			final AbstractClassMetaData acmd = ec.getMetaDataManager()
+					.getMetaDataForClass(candidateClass, clr);
+
+			Client c = (Client) mconn.getConnection();
+			String columnFamily = CassandraUtils.getFamilyName(acmd);
+			ColumnParent parent = new ColumnParent();
+			parent.setColumn_family(columnFamily);
+
+			SlicePredicate slice_predicate = new SlicePredicate();
+			int[] fieldNumbers = acmd.getAllMemberPositions();
+			List<byte[]> column_names = new ArrayList<byte[]>();
+			for (int i = 0; i < fieldNumbers.length; i++) {
+				byte[] columnName = CassandraUtils.getQualifierName(acmd,
+						fieldNumbers[i]).getBytes();
+				column_names.add(columnName);
+			}
+			slice_predicate.setColumn_names(column_names);
+
+			String last_key = "";
+			int number_keys = 0;
+			boolean terminated = false;
+
+			String limit = toExclNo;// (toExclNo<0) ? -1 : (toExclNo-1);
+			List<KeySlice> result = new ArrayList<KeySlice>();
+
+			KeyRange range = new KeyRange();
+			range.setStart_key(fromInclNo);
+			range.setEnd_key(toExclNo);
+			range.setCount(search_slice_ratio);
+
+			while (!terminated) {
+				List<KeySlice> keys = c.get_range_slices(keyspace, parent,
+						slice_predicate, range, ConsistencyLevel.QUORUM);
+
+				if (!keys.isEmpty()) {
+					range.setStart_key(keys.get(keys.size() - 1).key);
+				}
+
+				for (KeySlice key : keys) {
+					if (!key.getColumns().isEmpty()) {
+						number_keys++;
+						result.add(key);
+					}
+				}
+				if (keys.size() < search_slice_ratio) {
+					terminated = true;
+				}
+			}
+
+			Iterator<KeySlice> iterator = result.iterator();
+
+			while (iterator.hasNext()) {
+				final KeySlice keySlice = (KeySlice) iterator.next();
+				if (!keySlice.getColumns().isEmpty()) {
+
+					results.add(ec.findObjectUsingAID(new Type(clr
+							.classForName(acmd.getFullClassName())),
+							new FieldValues2() {
+
+								@Override
+								public FetchPlan getFetchPlanForLoading() {
+									return null;
+								}
+
+								@Override
+								public void fetchNonLoadedFields(
+										ObjectProvider sm) {
+									sm.replaceNonLoadedFields(acmd
+											.getAllMemberPositions(),
+											new CassandraFetchFieldManager(
+													acmd, sm, keySlice
+															.getColumns()));
+								}
+
+								@Override
+								public void fetchFields(ObjectProvider sm) {
+									sm.replaceFields(acmd
+											.getPKMemberPositions(),
+											new CassandraFetchFieldManager(
+													acmd, sm, keySlice
+															.getColumns()));
+									sm.replaceFields(acmd
+											.getBasicMemberPositions(clr, ec
+													.getMetaDataManager()),
+											new CassandraFetchFieldManager(
+													acmd, sm, keySlice
+															.getColumns()));
+
+								}
+							}, ignoreCache, true));
+
+				}
+			}
+		} catch (InvalidRequestException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		} catch (UnavailableException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		} catch (TimedOutException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		} catch (TException e) {
+			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
+		}
+		return results;
+	}
+
+	static List getObjectsOfCandidateType(final ExecutionContext ec,
+			final CassandraManagedConnection mconn, Class candidateClass,
+			boolean subclasses, boolean ignoreCache, long fromInclNo,
+			long toExclNo, String index_table, String value) {
+		List results = new ArrayList();
+
+		try {
+			String keyspace = ((CassandraStoreManager) ec.getStoreManager())
+					.getConnectionInfo().getKeyspace();
+			final ClassLoaderResolver clr = ec.getClassLoaderResolver();
+			final AbstractClassMetaData acmd = ec.getMetaDataManager()
+					.getMetaDataForClass(candidateClass, clr);
+
+			Client c = (Client) mconn.getConnection();
+			ColumnParent parent = new ColumnParent();
+			parent.setColumn_family(index_table);
+
+			SlicePredicate slice_predicate = new SlicePredicate();
+			
+			if(fromInclNo + toExclNo > Integer.MAX_VALUE){
+				toExclNo = Integer.MAX_VALUE-fromInclNo-1;
+			}
+			
+			SliceRange range = new SliceRange("".getBytes(), "".getBytes(),
+					false, ((int) (fromInclNo + toExclNo)));
+			slice_predicate.setSlice_range(range);
+
+			List<ColumnOrSuperColumn> key_columns = c.get_slice(keyspace,
+					value, parent, slice_predicate, ConsistencyLevel.QUORUM);
+			List<String> keys = new ArrayList<String>();
+
+			
+			int number_keys = 0;
+			for (ColumnOrSuperColumn index_column : key_columns) {
+				number_keys++;
+				if (number_keys > fromInclNo) {
+					byte[] column_name = index_column.getColumn().name;
+					String key;
+					try {
+						key = new String(column_name,"UTF8");
+						keys.add(key);
+					} catch (UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						continue;
+					}
+				
+				}
+			}
+
+			slice_predicate = new SlicePredicate();
+			int[] fieldNumbers = acmd.getAllMemberPositions();
+			List<byte[]> column_names = new ArrayList<byte[]>();
+			for (int i = 0; i < fieldNumbers.length; i++) {
+				byte[] columnName = CassandraUtils.getQualifierName(acmd,
+						fieldNumbers[i]).getBytes();
+				column_names.add(columnName);
+			}
+			slice_predicate.setColumn_names(column_names);
+
+			String columnFamily = CassandraUtils.getFamilyName(acmd);
+			parent = new ColumnParent();
+			parent.setColumn_family(columnFamily);
+
+			Map<String, List<ColumnOrSuperColumn>> result = new TreeMap<String, List<ColumnOrSuperColumn>>();
+
+			Map<String, List<ColumnOrSuperColumn>> entities = c.multiget_slice(
+					keyspace, keys, parent, slice_predicate,
+					ConsistencyLevel.QUORUM);
+
+			// List<KeySlice> entities = c.get_range_slice(keyspace, parent,
+			// slice_predicate, last_key, "", search_slice_ratio,
+			// ConsistencyLevel.QUORUM);
+
+			for (Entry<String, List<ColumnOrSuperColumn>> element : entities
+					.entrySet()) {
+
+				String key = element.getKey();
+				List<ColumnOrSuperColumn> columns = element.getValue();
+
+				if (!columns.isEmpty()) {
+					result.put(key, columns);
+				}
+			}
+
+			for (Entry<String, List<ColumnOrSuperColumn>> element : result
+					.entrySet()) {
+				
+				final List<ColumnOrSuperColumn> columns = element.getValue();
+
+				results.add(ec.findObjectUsingAID(new Type(clr
+						.classForName(acmd.getFullClassName())),
+						new FieldValues2() {
+
+							@Override
+							public FetchPlan getFetchPlanForLoading() {
+								return null;
+							}
+
+							@Override
+							public void fetchNonLoadedFields(ObjectProvider sm) {
+								sm.replaceNonLoadedFields(acmd
+										.getAllMemberPositions(),
+										new CassandraFetchFieldManager(acmd,
+												sm, columns));
+							}
+
+							@Override
+							public void fetchFields(ObjectProvider sm) {
+								sm.replaceFields(acmd.getPKMemberPositions(),
+										new CassandraFetchFieldManager(acmd,
+												sm, columns));
+								sm.replaceFields(acmd.getBasicMemberPositions(
+										clr, ec.getMetaDataManager()),
+										new CassandraFetchFieldManager(acmd,
+												sm, columns));
+
+							}
+						}, ignoreCache, true));
+
 			}
 		} catch (InvalidRequestException e) {
 			throw new NucleusDataStoreException(e.getMessage(), e.getCause());
--- src/org/datanucleus/store/cassandra/query/JDOQLQuery.java
+++ src/org/datanucleus/store/cassandra/query/JDOQLQuery.java
@@ -8,8 +8,16 @@ import java.util.Map;
 
 import org.datanucleus.ObjectManager;
 import org.datanucleus.exceptions.NucleusUserException;
+import org.datanucleus.metadata.AbstractClassMetaData;
+import org.datanucleus.metadata.AbstractMemberMetaData;
+import org.datanucleus.metadata.IndexMetaData;
 import org.datanucleus.query.evaluator.JDOQLEvaluator;
 import org.datanucleus.query.evaluator.JavaQueryEvaluator;
+import org.datanucleus.query.expression.Expression;
+import org.datanucleus.query.expression.Literal;
+import org.datanucleus.query.expression.PrimaryExpression;
+import org.datanucleus.query.expression.Expression.Operator;
+import org.datanucleus.query.symbol.Symbol;
 import org.datanucleus.store.ExecutionContext;
 
 import org.datanucleus.store.cassandra.CassandraManagedConnection;
@@ -57,14 +65,13 @@ public class JDOQLQuery extends AbstractJDOQLQuery {
 	@Override
 	protected Object performExecute(Map parameters) {
 
+		String start_key = null;
+		String end_key = null;;
+		
 		if (range != null && !range.equals("")) {
 			// Range is of the format "from, to"
 			String[] fromTo = range.split(",");
-			if (fromTo.length != 2) {
-				throw new NucleusUserException("Malformed RANGE clause: "
-						+ range);
-			}
-
+			if (fromTo.length == 2) {
 			try {
 				fromInclNo = Long.parseLong(fromTo[0].trim());
 			} catch (Exception e) {
@@ -78,10 +85,91 @@ public class JDOQLQuery extends AbstractJDOQLQuery {
 				throw new NucleusUserException("Malformed RANGE clause: "
 						+ range);
 			}
+			}else{
+				fromTo = range.split("to");
+				if (fromTo.length != 2) {
+					throw new NucleusUserException("Malformed RANGE clause: "
+							+ range);					
+				}
+				else{
+					start_key = fromTo[0].trim(); 
+					end_key = fromTo[1].trim();			
+				}
+			}
 
 		}
 		//		    
+		Expression exp =  this.compilation.getExprFilter();
 
+		if(exp!=null){
+			
+			Operator op = exp.getOperator();	
+			if(!op.toString().equals(" = ")){
+				throw new NucleusUserException("Only simple \"=\" operations are allowed at this phase");	
+			}
+			PrimaryExpression expression = (PrimaryExpression) exp.getLeft();
+			String column = expression.getSymbol().getQualifiedName();
+			
+			Literal literal = (Literal) exp.getRight();			
+			Object o = literal.getLiteral();
+			
+			AbstractClassMetaData metadata = ec.getMetaDataManager().getMetaDataForClass(candidateClass, ec.getClassLoaderResolver());
+			AbstractMemberMetaData memberMetaData =  metadata.getMetaDataForMember(column);
+			if(memberMetaData==null){
+				throw new NucleusUserException("Unknown field on query");	
+			}
+			IndexMetaData indexmd = memberMetaData.getIndexMetaData();
+			if(indexmd==null){
+				throw new NucleusUserException("The used field must have a index");	
+			}
+			
+			String index_table= indexmd.getTable();
+			
+			CassandraManagedConnection mconn = (CassandraManagedConnection) ec
+					.getStoreManager().getConnection(ec);
+			try {
+				long startTime = System.currentTimeMillis();
+				if (NucleusLogger.QUERY.isDebugEnabled()) {
+					NucleusLogger.QUERY.debug(LOCALISER.msg("021046", "JDOQL",
+							getSingleStringQuery(), null));
+				}
+				List candidates = null;
+				if (candidateCollection != null) {
+					candidates = new ArrayList(candidateCollection);
+				} 
+	//				else if (candidateExtent != null) {
+//					candidates = new ArrayList();
+//					Iterator iter = candidateExtent.iterator();
+//					while (iter.hasNext()) {
+//						candidates.add(iter.next());
+//					}
+//				}
+			//		else {
+					candidates = CassandraQuery.getObjectsOfCandidateType(ec,
+							mconn, candidateClass, subclasses, ignoreCache,
+							fromInclNo, toExclNo,index_table,(String)o);
+				//}
+
+				// Apply any result restrictions to the results
+				JavaQueryEvaluator resultMapper = new JDOQLEvaluator(this,
+						candidates, compilation, parameters, ec
+								.getClassLoaderResolver());
+				Collection results = resultMapper.execute(false, true, true,
+						true, false);
+
+				if (NucleusLogger.QUERY.isDebugEnabled()) {
+					NucleusLogger.QUERY.debug(LOCALISER.msg("021074", "JDOQL",
+							"" + (System.currentTimeMillis() - startTime)));
+				}
+
+				return results;
+			} finally {
+				mconn.release();
+			}
+			
+			
+		}
+		
 		CassandraManagedConnection mconn = (CassandraManagedConnection) ec
 				.getStoreManager().getConnection(ec);
 		try {
